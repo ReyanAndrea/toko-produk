a9dcdc8a75425d524780782fbbbb81fcfb8e058a
ReyanAndrea <reyanandre@gmail.com>
2026-05-14 16:21:57 +0700

    Add ProdukController with index method

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 
+// Controller untuk mengelola data produk toko
 class ProdukController extends Controller
 {
+    // Menampilkan semua data produk dari database
     public function index()
     {
         $produks = Produk::all();
